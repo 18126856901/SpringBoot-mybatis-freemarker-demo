ad117670a15c94f214a9ccb9c4ceb3a16d552408
thinking <861958334@qq.com>
2017-06-15 16:47:44 +0800

    首次提交

---
+++ src/main/java/com/example/demo/mapper/CityMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.pojo.City;
+
+@Mapper
+public interface CityMapper {
+	
+	List<City> selectAll();
+	
+}
